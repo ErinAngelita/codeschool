d3ca21ca7bef42982121e79ce207dc850b901cd7
erin olsen <erinolsen@LAPTOP-C7LRC9L0.localdomain>
2018-02-04 17:19:28 -0700

    tryin agian

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/',express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => console.log("Served on" + port));
+app.listen(port, () => console.log("Served on" + port));
